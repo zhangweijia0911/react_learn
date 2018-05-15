a4b333e8e1fb5a3d14086ea4eab32a3f206a74e0
zhangwjr <ufida2013%>
2018-05-15 19:35:41 +0800

    Todo留言板

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {createStore} from 'redux';
 const ADD_TODO = 'ADD_TODO';//这是action的类型
 const DEL_TODO = 'DEL_TODO';//这是action的类型
+
 let reducer = (state={list:[]},action)=>{
     if(action === undefined) return state;
     switch (action.type){
@@ -9,31 +10,27 @@ let reducer = (state={list:[]},action)=>{
             return {list:[...state.list,action.text]};
         case DEL_TODO:
             let list = state.list;
-            list.splice(action.index);
+            list.splice(action.index,1);
             //我们的状态具有不变性，每次都要返回一个新的对象
             return {list:[...list]};
         default:
             return state;
     }
 };
-let store = createStore(reducer());
+let store = createStore(reducer);
 
-class Todo extends Component {
+export default class Todo extends Component {
     constructor(props){
         super(props);
         this.state = {list:store.getState().list}
     }
     handleKeyDown = (event)=>{
-        if(event.keyCode == 13 && event.target.value.length>0){
+        if(event.keyCode === 13 && event.target.value.length>0){
             store.dispatch({
                 type:ADD_TODO,
                 text:event.target.value
             })
             event.target.value = '';
-            /*let list = this.state.list;
-            list.push(event.target.value);
-            this.setState({list});
-            event.target.value = '';*/
         }
     };
     deleteTodo(index){
@@ -43,11 +40,15 @@ class Todo extends Component {
         })
     }
     componentWillMount(){
-        store.subscribe(()=>{
+        //订阅
+        this.unSubscribe = store.subscribe(()=>{
             this.setState({
                 list:store.getState().list
             })
         })
+    }
+    componentWillUnmount(){
+        this.unSubscribe();
     }
     render(){
         return (
@@ -55,11 +56,14 @@ class Todo extends Component {
                 <input type="text" onKeyDown={this.handleKeyDown}/>
                 <ul>
                     {
-                        this.state.list.map((todo,index)=>(<li key={index}>{todo}<button onClick={()=>this.deleteTodo(index)}>-</button></li>))
+                        this.state.list.map((todo,index)=>{
+                            return (
+                                <li key={index}>{todo}<button onClick={()=>this.deleteTodo(index)}>-</button></li>
+                            )
+                        })
                     }
                 </ul>
             </div>
         )
     }
 }
-export default Todo;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 //import Counter from './components/Counter';
 import Todo from './components/Todo';
+
 ReactDOM.render(<Todo/>,window.app);
 
